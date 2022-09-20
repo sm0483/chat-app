7d69fe88c41824e9e387a4f1bc4eb3e13f179807
Sreerag <msratofficial@gmail.com>
2022-09-20 11:49:43 +0530

    feat: create a login in page and add css

---
+++ src/componet/Login.js
@@ -0,0 +1,47 @@
+import { Button } from "react-bootstrap";
+import {FcGoogle} from 'react-icons/fc'
+import login from '../images/image.svg'
+
+const Login = () => {
+    return (
+        <section className="login-container">
+            <div className="main-head">
+                <div className="heading">
+                    <h1>Login</h1>
+                    <h2>See your friends</h2>
+                </div>
+                <div className="button-container">
+                    <Button><FcGoogle/></Button>
+                </div>
+
+                <div className="details-container">
+                    <form >
+                        <label htmlFor="id">Email</label>
+                        <input type="email" id="id" placeholder="abc@gmail.com"/>
+                        <label htmlFor="id">Password</label>
+                        <input type="password" id="id" placeholder="abc@gmail.com"/>
+                        <div className="details-footer">
+                            <input type="checkbox" id='id' />
+                            <label htmlFor="id">Remember me</label>
+                            <a href="#forget">Forget Password</a>
+                        </div>
+                        <Button type="submit">Login</Button>
+                    </form>
+
+                    <div className="login-footer">
+                        <label >New to Chat? </label>
+                        <a>SIGN UP</a>
+                    </div>
+                </div>
+         
+            </div>
+
+            <div className="image-container">
+                    <img src={login} className="img-fluid" alt="login image" />
+                </div>
+
+        </section>
+    );
+}
+ 
+export default Login;
---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAp_-4pJOHRMtzeXhJym6YD20tALQOQNLw",
+  authDomain: "chatapp-5ca4f.firebaseapp.com",
+  projectId: "chatapp-5ca4f",
+  storageBucket: "chatapp-5ca4f.appspot.com",
+  messagingSenderId: "695848502808",
+  appId: "1:695848502808:web:7748d94d1954ed56ea256d",
+  measurementId: "G-G3Q5NFKQ9J"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
