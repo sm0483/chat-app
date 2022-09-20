c067a67ee1ff98f3826fcd185abb2aa7ec738e50
Sreerag <msratofficial@gmail.com>
2022-09-20 21:20:35 +0530

    feat:setup authentication in register page

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,3 +1,47 @@
 import {auth} from '../firebase'
-import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { createUserWithEmailAndPassword,signInWithPopup,GoogleAuthProvider } from 'firebase/auth'
+import { useContext, useState } from 'react'
+import React from 'react';
 
+const AuthContext=React.createContext();
+
+const AuthProvider=({children})=>{
+    const [logedIn,setLogedIn]=useState(false);
+    const [err,setErr]=useState(false);
+
+    const createUserEmail=async(email,password)=>{
+        try{
+            return await createUserWithEmailAndPassword(auth,email,password);
+        }catch(err){
+            setErr(true);
+        }
+    }
+    const provider=new GoogleAuthProvider();
+
+    const loginWithGoogle=async()=>{
+        try{
+         return await signInWithPopup(auth,provider);
+         setErr(false)
+        }catch(err){
+            setErr(true);
+        }
+    }
+
+    return(
+        <AuthContext.Provider
+        value={{
+            createUserEmail,
+            loginWithGoogle
+        }}
+        >
+            {children}
+        </AuthContext.Provider>
+    )
+
+}
+
+const useAuthContext=()=>{
+    return useContext(AuthContext);
+}
+
+export  {useAuthContext,AuthProvider,AuthContext}
